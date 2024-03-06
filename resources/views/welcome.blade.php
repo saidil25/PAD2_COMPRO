808ac986f6a51c83cd948e9753143544032b0888
saidilhalim <saidilhalim@mail.ugm.ac.id>
2024-03-06 14:10:14 +0700

    commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline text-red-600">
+    Hello world!
+  </h1>
+
+  <h2 class="text-4xl items-center text-blue-400  ">
+    aku disini
+  </h2>
+  
+</body>
+</html>
